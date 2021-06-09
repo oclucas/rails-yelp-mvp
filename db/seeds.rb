0a217ffee9657d3b95fa38fd2f323f9a26085a58
Oceane Lucas <oceane.lucas@edhec.com>
2021-06-09 15:43:59 +0200

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+type = %w[chinese italian japanese french belgian]
+
+5.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name,
+                              category: type.sample,
+                              phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+                              address: Faker::Address.full_address)
+  restaurant.save
+end
